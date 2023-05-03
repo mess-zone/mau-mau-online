3cae3610a96f918ca9585fffb8a7e49f6d9f664c
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-05-03 17:26:09 -0300

    feat [server]:  Room entity

--- apps/mau-mau-online-server/src/entities/room.ts
+++ apps/mau-mau-online-server/src/entities/room.ts
@@ -1,5 +1,9 @@
+import { ArrayList } from "@mess-zone/mau-mau-online-core"
+import { User } from "./user"
+
 export type RoomConfig = {
-    id: string
+    id: string,
+    list: ArrayList<User>
 }
 
 /**
@@ -7,12 +11,41 @@ export type RoomConfig = {
  */
 export class Room {
     private readonly id: string
+    private readonly users: ArrayList<User>
 
     constructor({id}: RoomConfig) {
         this.id = id
+        this.users = new ArrayList<User>()
     }
 
     public getId() {
         return this.id
+    }
+
+    public addUser(user: User) {
+        this.users.add(user)
+    }
+
+    public removeUser(id: string) {
+        const user = this.getById(id)
+        return this.users.remove(user)   
+    }
+
+    public getById(id: string) {
+        for(const user of this.iterator()) {
+            if(user.id === id) {
+                return user
+            }
+        }
+
+        return undefined
+    }
+
+    public size() {
+        return this.users.size()
+    }
+
+    public *iterator() {
+        yield* this.users.iterator()
     }
 }
--- apps/mau-mau-online-server/test/entities/room.spec.ts
+++ apps/mau-mau-online-server/test/entities/room.spec.ts
@@ -1,16 +1,141 @@
-import { Room } from "@/entities"
+import { Room, User } from "@/entities"
+import { ArrayList } from "@mess-zone/mau-mau-online-core";
+
+jest.mock('@mess-zone/mau-mau-online-core');
+let mockedList = jest.mocked(ArrayList);
 
 describe('Room entity', () => {
+    let list: ArrayList<User>;
+
     let room : Room
     beforeEach(() => {
-        room = new Room({ id: 'room-id' })
+        jest.clearAllMocks()
+        list = new ArrayList<User>()
+        room = new Room({ id: 'room-id', list })
     })
 
     test('deve ter um id', () => {
         expect(room.getId()).toBe('room-id')
     })
 
-    test.todo('deve permitir adicionar usuário')
-    test.todo('deve permitir remover usuário')
+    test('deve permitir adicionar usuários', () => {
+        const user0: User = {
+            id: "user0",
+            name: "user 0",
+            color: "#456456",
+            icon: "icon url"
+        }
+        const user1: User = {
+            id: "user1",
+            name: "user 1",
+            color: "#456456",
+            icon: "icon url"
+        }
+
+        room.addUser(user0)
+        expect(mockedList.prototype.add).toBeCalledWith(user0)
+        room.addUser(user1)
+        expect(mockedList.prototype.add).toBeCalledWith(user1)
+    })
+
+    test('deve permitir remover usuário', () => {
+        const user0: User = {
+            id: "user0",
+            name: "user 0",
+            color: "#456456",
+            icon: "icon url"
+        }
+        const user1: User = {
+            id: "user1",
+            name: "user 1",
+            color: "#456456",
+            icon: "icon url"
+        }
+        const user2: User = {
+            id: "user2",
+            name: "user 2",
+            color: "#456456",
+            icon: "icon url"
+        }
+
+        mockedList.prototype.iterator.mockImplementation(function* () {
+            yield user0
+            yield user1
+            yield user2
+        })
+        mockedList.prototype.remove.mockReturnValue(user1)
+
+        const removedUser = room.removeUser('user1')
+        expect(mockedList.prototype.remove).toBeCalledWith(user1)
+        expect(removedUser).toEqual(user1)
+    })
+
+    test('deve permitir encontrar um usuario pelo id', () => {
+        const user0: User = {
+            id: "user0",
+            name: "user 0",
+            color: "#456456",
+            icon: "icon url"
+        }
+        const user1: User = {
+            id: "user1",
+            name: "user 1",
+            color: "#456456",
+            icon: "icon url"
+        }
+        const user2: User = {
+            id: "user2",
+            name: "user 2",
+            color: "#456456",
+            icon: "icon url"
+        }
+
+        mockedList.prototype.iterator.mockImplementation(function* () {
+            yield user0
+            yield user1
+            yield user2
+        })
+
+        const result = room.getById('user1')
+        expect(result).toEqual(user1)
+    })
+
+    test('deve retornar undefined se não econtrar um usuario pelo id', () => {
+        const result = room.getById('invalid-user-id')
+        expect(result).toEqual(undefined)
+    })
+
+    test('deve listar todos os usuarios', () => {
+        const user0: User = {
+            id: "user0",
+            name: "user 0",
+            color: "#456456",
+            icon: "icon url"
+        }
+        const user1: User = {
+            id: "user1",
+            name: "user 1",
+            color: "#456456",
+            icon: "icon url"
+        }
+        const user2: User = {
+            id: "user2",
+            name: "user 2",
+            color: "#456456",
+            icon: "icon url"
+        }
+
+        mockedList.prototype.iterator.mockImplementation(function* () {
+            yield user0
+            yield user1
+            yield user2
+        })
+
+        const users = [...room.iterator()]
+        expect(users).toHaveLength(3)
+        expect(users[0]).toEqual(user0)
+        expect(users[1]).toEqual(user1)
+        expect(users[2]).toEqual(user2)
+    })
 
 })
